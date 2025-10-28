3a3646cba8fd0851ba33997e88d373a8ba8908af
Jashvarthini Rajesh <jashvarthinirajesh@gmail.com>
2025-10-28 14:14:16 +0530

    Fix: CORS error

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,3 +1,43 @@
+// import { Server } from "socket.io";
+// import http from "http";
+// import express from "express";
+
+// const app = express();
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: ["http://localhost:5173",
+//       "https://chat-frontend-psi-eight.vercel.app"
+//     ],
+//   },
+// });
+
+// export function getReceiverSocketId(userId) {
+//   return userSocketMap[userId];
+// }
+
+// // used to store online users
+// const userSocketMap = {}; // {userId: socketId}
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.id);
+
+//   const userId = socket.handshake.query.userId;
+//   if (userId) userSocketMap[userId] = socket.id;
+
+//   // io.emit() is used to send events to all the connected clients
+//   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected", socket.id);
+//     delete userSocketMap[userId];
+//     io.emit("getOnlineUsers", Object.keys(userSocketMap));
+//   });
+// });
+
+// export { io, app, server };
+
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -7,18 +47,21 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173",
-      "https://chat-frontend-psi-eight.vercel.app"
+    origin: [
+      "http://localhost:5173",
+      "https://chat-frontend-psi-eight.vercel.app",
+      "https://chat-frontend-omega-navy.vercel.app"
     ],
   },
 });
 
+// store online users
+const userSocketMap = {}; // {userId: socketId}
+
+// helper
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -26,7 +69,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
